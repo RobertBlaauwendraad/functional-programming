20b1fdbdd220d57acc4bbd401c18fafbdbe9906a
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-09-13 13:58:40 +0200

    Finish exercise 2.5

--- assignment-2/Char.hs
+++ assignment-2/Char.hs
@@ -2,13 +2,21 @@ module Char where
 
 import Data.Char
 
---(~~) :: String -> String -> Bool
+(~~) :: String -> String -> Bool
+(~~) n m = map toLower n == map toLower m
 
---reverseCase :: String -> String
+reverseCase :: String -> String
+reverseCase = map (\c -> if isUpper c then toLower c else toUpper c)
 
---shift :: Int -> Char -> Char
+shift :: Int -> Char -> Char
+shift n c
+  | isUpper c && isAscii c = chr $ ord 'A' + (ord c - ord 'A' + n) `mod` 26
+  | otherwise = c
 
---caesar :: Int -> String -> String
+caesar :: Int -> String -> String
+caesar n s = map (shift n) (map toUpper s)
 
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
+-- caesar 5 msg gives:
+-- "FIRST I MUST SPRINKLE YOU WITH FAIRY DUST"
