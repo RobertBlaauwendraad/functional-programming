32de60827a38f647e4be5255946d04bb8898d635
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-09-13 13:36:39 +0200

    Finish exercise 2.4

--- assignment-2/PolyType.hs
+++ assignment-2/PolyType.hs
@@ -10,3 +10,11 @@ f10 x y
 
 f11 x y = get 0
   where get n = if n == 0 then x else y
+
+-- a)
+-- f8 and f11 can be used on arguments of type String
+-- b)
+-- f8 is ad-hoc polymorphic
+-- f9 is not polymorphic
+-- f10 is not polymorphic
+-- f11 is parametric polymorphic
