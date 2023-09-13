a92b20cf42c4839537050f426310126d4dc03c63
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-09-13 11:50:12 +0200

    Finish exercise 2.3

--- assignment-2/Pow2.hs
+++ assignment-2/Pow2.hs
@@ -1,9 +1,10 @@
 module Pow2 where
 
-pow2 :: Integer -> Integer
+pow2 :: (Ord n, Num n, Num a) => n -> a
 pow2 0 = 1
--- pow2 n = ... recursive case
+pow2 n = 2 * pow2 (n-1)
 
-
---after finishing the function, change the type to: 
---pow2 :: (Ord n, Num n, Num a) => n -> a
+-- Maximum n for Integer: 500000 still worked so probably really high/infinity depending on your CPU/Memory and patience
+-- Maximum n for Int: 62, (63 gave a negative number and > 63 gives 0)
+-- Maximum n for Float: 127, (128 gave infinity)
+-- Maximum n for Double: 1023, (1025 gave infinity)
