e2a6a5f416f7e9759e4c8c959e8ac3592fbf5609
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-09-22 14:30:24 +0200

    Finish 3.7

--- assignment-3/Lego.hs
+++ assignment-3/Lego.hs
@@ -3,8 +3,11 @@ module Lego where
 import Data.List
 import Data.Tuple
 
---removeAt :: Int -> [a] -> [a]
+removeAt :: Int -> [a] -> [a]
+removeAt n xs = [ x | (i,x) <- zip [0..] xs, i /= n ]
 
---sortWithPos :: (Ord a) => [a] -> [(a,Int)]
+sortWithPos :: (Ord a) => [a] -> [(a,Int)]
+sortWithPos xs = sort [(x,i)| (x,i) <- zip xs [0..]]
 
---sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos xs = [(x,j)|((i,x),j) <- sort[((i,x),j)| ((x,i),j) <- zip (sortWithPos xs) [0..]]]
