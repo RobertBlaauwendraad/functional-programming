d1631fce43685816b40b47d708ec184df55df1d0
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-09-22 14:13:24 +0200

    Finish 3.5 and 3.6

--- assignment-3/ListComprehensions.hs
+++ assignment-3/ListComprehensions.hs
@@ -1,13 +1,31 @@
 module ListComprehensions where
 
+g0 :: [a] -> [b] -> [(a,b)]
 g0 as bs = [ (a,b) | a <- as, b <- bs ]
+-- cartesianProduct, combines every element of as with every element of bs
+-- Polymorphic
 
+g1 :: Int -> a -> [a]
 g1 n y   = [ y | i <- [1..n] ]
+-- replicate, creates a list of n elements, all equal to y
+-- Polymorphic and overloaded function
 
+g2 :: Int -> [a] -> [a]
 g2 n xs  = [ x | (i,x) <- zip [0..] xs, i < n ]
+-- take, takes the first n elements of xs
+-- Polymorphic and overloaded function
 
+g3 :: (Eq a) => a -> [a] -> [Int]
 g3 a xs  = [ i | (i,x) <- zip [0..] xs, x == a]
+-- findIndices, finds the indices of all elements in xs that are equal to a
+-- Overloaded function
 
+g4 :: [a] -> [a] -> [a]
 g4 xs ys = [ e | (x,y) <- zip xs ys, e <- [x,y] ]
+-- interleave, interleaves the elements of xs and ys
+-- Polymorphic function
 
+g5 :: [[a]] -> [a]
 g5 xss   = [ x | xs <- xss, x <- xs ]
+-- concat, concatenates the elements of xss
+-- Polymorphic function
--- assignment-3/Uniq.hs
+++ assignment-3/Uniq.hs
@@ -1,3 +1,6 @@
 module Uniq where
 
---uniq :: (Eq a) => [a] -> [a]
+uniq :: (Eq a) => [a] -> [a]
+uniq [] = []
+uniq [x] = [x]
+uniq (x:y:xs) = if x == y then uniq (y:xs) else x : uniq (y:xs)
