acfe87112f2bd2f5b147bb02e9ba818c785c2a08
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-10-08 18:45:04 +0200

    Start of 5.7

--- assignment-5/Unfold.hs
+++ assignment-5/Unfold.hs
@@ -4,13 +4,19 @@ import Data.List (unfoldr)
 import Prelude hiding (take,zip,(++))
 
 -- define all the below funtions using `unfoldr`
--- bits :: Int -> [Int]
--- zip :: [a] -> [b] -> [(a,b)]
--- take :: Int -> [a] -> [a]
--- primes :: [Integer]
+bits :: Int -> [Int]
+bits = unfoldr (\x -> if x == 0 then Nothing else Just (x `mod` 2, x `div` 2))
+
+zip :: [a] -> [b] -> [(a,b)]
+zip xs ys = unfoldr (\(x:xs,y:ys) -> Just ((x,y), (xs,ys))) (xs,ys)
+
+--take :: Int -> [a] -> [a]
+primes :: [Integer]
+primes = unfoldr (\(x:xs) -> Just (x, filter (\y -> y `mod` x /= 0) xs)) [2..]
+
 -- alternative implementation of `primes`:
-primes' = sieve [2..]
-  where sieve (p:xs) = p : sieve [ n | n <- xs, n ‘mod‘ p /= 0 ]
+--primes' = sieve [2..]
+--  where sieve (p:xs) = p : sieve [ n | n <- xs, n ‘mod‘ p /= 0 ]
 
 apo :: (t -> Either [a] (a, t)) -> t -> [a]
 apo f seed = case f seed of
