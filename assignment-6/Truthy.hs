a73601ffa24c90ef7904909e1d755673ebdc11a8
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2023-10-15 13:27:25 +0200

    Finish 6.5

--- assignment-6/Truthy.hs
+++ assignment-6/Truthy.hs
@@ -1,12 +1,30 @@
 module Truthy where
 
--- class Truthy ...
+class Truthy a where
+    truthy :: a -> Bool
 
--- instance Truthy ... where
+instance Truthy Bool where
+    truthy = id
 
--- infixr 3 &&&
--- infixr 2 |||
--- (&&&) :: ...
--- (|||) :: ...
---- ifThenElse :: ...
+instance Truthy Integer where
+    truthy = (/= 0)
 
+data Nope = Nope
+
+instance Truthy Nope where
+    truthy _ = False
+
+instance (Truthy a, Truthy b) => Truthy (a, b) where
+    truthy (a, b) = truthy a || truthy b
+
+infixr 3 &&&
+infixr 2 |||
+
+(&&&) :: (Truthy a, Truthy b) => a -> b -> Bool
+(&&&) a b = truthy a && truthy b
+
+(|||) :: (Truthy a, Truthy b) => a -> b -> Bool
+(|||) a b = truthy a || truthy b
+
+ifThenElse :: Truthy p => p -> a -> a -> a
+ifThenElse p a b = if truthy p then a else b
